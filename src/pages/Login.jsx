e4c22ee637605b289a6d1493513b3a49fd272a82
Luiz Fernando Heilig <luiz.heilig@hotmail.com>
2023-05-22 17:44:55 -0300

    requisito 2 e 3

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   state = {
@@ -7,28 +8,44 @@ class Login extends Component {
     isDisabled: true,
   };
 
-  // função que habilita/desabilita o botão 
+  // função que habilita/desabilita o botão
   isDisabled = () => {
-    const { name, email } = this.state;    
+    const { name, email } = this.state;
     const validEmail = /^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/;
     const two = 2;
 
-    if(email.match(validEmail) && name.length > two){
+    if (email.match(validEmail) && name.length > two) {
       this.setState({
         isDisabled: false,
-      })
+      });
     } else {
       this.setState({
         isDisabled: true,
-      })
+      });
     }
+  };
+
+  // função que redireciona a partir do botão play
+  onClickButtonPlay = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(login({ email }));
+    history.push('/game');
+  };
+
+  // função que redireciona a partir do botão configuracoes
+  onClickButtonConfig = (event) => {
+    event.preventDefault();
+    const { history } = this.props;
+    history.push('/config');
   };
 
   // função que salva os dados de nome, email e disabled do botão no estado local da página
   handleChange = ({ target: { name, value } }) => {
     this.setState({
       [name]: value,
-    }, this.isDisabled)
+    }, this.isDisabled);
   };
 
   render() {
@@ -42,34 +59,48 @@ class Login extends Component {
               type="email"
               name="email"
               id="input-email"
-              placeholder='Digite seu e-mail'
-              data-testid='input-gravatar-email'
+              placeholder="Digite seu e-mail"
+              data-testid="input-gravatar-email"
               onChange={ this.handleChange }
             />
           </label>
-          
+
           <label htmlFor="input-name">
             <input
               type="text"
               name="name"
               id="input-name"
-              placeholder='Digite seu nome'
-              data-testid='input-player-name'
+              placeholder="Digite seu nome"
+              data-testid="input-player-name"
               onChange={ this.handleChange }
             />
           </label>
 
           <button
-            type='button'
+            type="button"
             disabled={ isDisabled }
-            data-testid='btn-play'
+            data-testid="btn-play"
+            onClick={ this.onClickButtonPlay }
           >
             Play
           </button>
+          <button
+            type="button"
+            data-testid="btn-settings"
+            onClick={ this.onClickButtonConfig }
+          >
+            Configurações
+          </button>
         </form>
       </div>
-    )
+    );
   }
-};
+}
 
-export default Login
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
+
+export default Login;
