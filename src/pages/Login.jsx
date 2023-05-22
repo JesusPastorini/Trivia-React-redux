f8348397acc4fd2f703d8b4065343c8dfba0b5ae
Nicolas Lima <nicolaslima1500@gmail.com>
2023-05-22 16:08:44 -0300

    Corrigido o erro de start do projeto

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,15 @@ class Login extends Component {
   render() {
     return (
       <div>
-        
+        <form action="">
+          <label htmlFor="input-name">
+            <input type="text" name="name" id="input-name" />
+          </label>
+
+          <label htmlFor="input-email">
+            <input type="email" name="email" id="input-email" />
+          </label>
+        </form>
       </div>
     )
   }
