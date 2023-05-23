722e2b11f203f8285db60782dddc09595b0896d8
Luiz Fernando Heilig <luiz.heilig@hotmail.com>
2023-05-23 16:17:21 -0300

    requisitos 12, 15 e 16

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,53 @@
 import React from 'react';
+import md5 from 'crypto-js/md5';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Feedback extends React.Component {
+  onClickButtonPlayAgain = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  onClickButtonRanking = () => {
+    const { history } = this.props;
+    history.push('/ranking');
+  };
+
   render() {
+    const { email, name, score } = this.props;
+    const hashEmail = md5(email).toString();
+
     const { assertions } = this.props;
     const low = 3;
 
     return (
       <div>
+        <img
+          data-testid="header-profile-picture"
+          src={ `https://www.gravatar.com/avatar/${hashEmail}` }
+          alt="profile-gravatar"
+        />
+        <p data-testid="header-player-name">{name}</p>
+        <p data-testid="header-score">{score}</p>
+
         <p data-testid="feedback-text">
           { assertions < low ? 'Could be better...' : 'Well Done!' }
         </p>
+        <button
+          type="button"
+          data-testid="btn-play-again"
+          onClick={ this.onClickButtonPlayAgain }
+        >
+          Play Again
+        </button>
+        <button
+          type="button"
+          data-testid="btn-ranking"
+          onClick={ this.onClickButtonRanking }
+        >
+          Ranking
+        </button>
       </div>
     );
   }
@@ -19,10 +55,19 @@ class Feedback extends React.Component {
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  email: state.player.email,
+  name: state.player.name,
+  score: state.player.score,
 });
 
 Feedback.propTypes = {
   assertions: PropTypes.number.isRequired,
+  email: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
